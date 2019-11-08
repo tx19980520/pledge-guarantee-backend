d97fadfbf6077da6a6e2f25ff50aab1b217b0679
ty0207 <ty0207@sjtu.edu.cn>
2019-11-08 14:37:02 +0800

    init spring
    init spring

---
+++ src/test/java/wd/pledge/guarantee/GuaranteeApplicationTests.java
@@ -0,0 +1,13 @@
+package wd.pledge.guarantee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuaranteeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
