4f865aef845fa5a9f381ab53c07c1bc3ef7941e2
Bert-Z <wanqiangzhang@sjtu.edu.cn>
2019-11-11 22:35:39 +0800

    redis config and add test

--- src/main/java/wd/pledge/guarantee/GuaranteeApplication.java
+++ src/main/java/wd/pledge/guarantee/GuaranteeApplication.java
@@ -2,8 +2,13 @@ package wd.pledge.guarantee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
 
 @SpringBootApplication
+@EnableCaching
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600 * 24)
 public class GuaranteeApplication {
 
 	public static void main(String[] args) {
